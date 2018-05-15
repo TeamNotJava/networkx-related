cd71574d6904d51c52ad215cfd6d672343d8eb26
Valkum <rudi.floren@gmail.com>
2018-05-15 17:44:25 +0200

    Add copyright and author comments on top of the boltzman package files.

--- planarity_generation/boltzman_sampler/__init__.py
+++ planarity_generation/boltzman_sampler/__init__.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+#    Copyright (C) 2018 by
+#    Marta Grobelna <marta.grobelna@rwth-aachen.de>
+#    Petre Petrov <petrepp4@gmail.com>
+#    Rudi Floren <rudi.floren@gmail.com>
+#    Tobias Winkler <tobias.winkler1@rwth-aachen.de>
+#    All rights reserved.
+#    BSD license.
+#
+# Authors:  Marta Grobelna <marta.grobelna@rwth-aachen.de>
+#           Petre Petrov <petrepp4@gmail.com>
+#           Rudi Floren <rudi.floren@gmail.com>
+#           Tobias Winkler <tobias.winkler1@rwth-aachen.de>
 __all__ = ["binary_tree", "three_connected_graph", "utils"]
--- planarity_generation/boltzman_sampler/three_connected_graph.py
+++ planarity_generation/boltzman_sampler/three_connected_graph.py
@@ -1,5 +1,16 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+#    Copyright (C) 2018 by
+#    Marta Grobelna <marta.grobelna@rwth-aachen.de>
+#    Petre Petrov <petrepp4@gmail.com>
+#    Rudi Floren <rudi.floren@gmail.com>
+#    Tobias Winkler <tobias.winkler1@rwth-aachen.de>
+#    All rights reserved.
+#    BSD license.
+#
+# Authors:  Marta Grobelna <marta.grobelna@rwth-aachen.de>
+#           Petre Petrov <petrepp4@gmail.com>
+#           Rudi Floren <rudi.floren@gmail.com>
+#           Tobias Winkler <tobias.winkler1@rwth-aachen.de>
 
 from .utils import bern_choice
 from .binary_tree import BinaryTreeSampler
