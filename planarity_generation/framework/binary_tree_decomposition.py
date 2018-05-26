e53d14cd66b4d868baca099852ee7937417d0580
Valkum <rudi.floren@gmail.com>
2018-05-26 20:51:29 +0200

    Add plotting to framework version

--- planarity_generation/framework/binary_tree_decomposition.py
+++ planarity_generation/framework/binary_tree_decomposition.py
@@ -14,26 +14,246 @@ R_b = Alias('R_b')
 R_w = Alias('R_w')
 
 class BinaryTree():
+    _id = 0
 
-    def __init__(self, left, right):
+    def __init__(self, left, right, **kwargs):
         self.left = left
         self.right = right
+        self.attributes = kwargs
+        self._id = BinaryTree._id
+        BinaryTree._id += 1
 
-def decomp_to_binary_tree_b(decomp):
-    print('test: '+decomp[0])
-    return BinaryTree(None, None)
+    def get_attribute(self, key):
+        return self.attributes[key]
 
-def decomp_to_binary_tree_w(decomp):
-    print('test: '+decomp[0])
-    return BinaryTree(None, None)
+    def get_attributes(self):
+        return self.attributes
+
+    def __repr__(self):
+        repr = '['
+        if self.left is None:
+            repr += '0'
+        else:
+            repr += str(self.left)
+
+        if 'color' in self.attributes:
+            if self.attributes['color'] is 'white':
+                repr += 'w'
+            if self.attributes['color'] is 'black':
+                repr += 'b'
+                
+        if self.right is None:
+            repr += '0'
+        else:
+            repr += str(self.right)
+        repr += ']'
+        return repr
+
+    
+    def pretty(self, indent='', direction='updown'):
+        label = "{0} ({1})".format(self._id, self.get_attribute('color'))
+        if type(self.right) is BinaryTree:
+            next_indent = '{0}{1}{2}'.format(indent, ' ' if 'up' in direction else '│', ' ' * len(label))
+            self.right.pretty(next_indent, direction='up')
+        else:
+            print('{0}{1}{2}┌-'.format(indent, ' ' if 'up' in direction else '│', ' ' * len(label)))
+
+        if direction == 'up': start_shape = '┌'
+        elif direction == 'down': start_shape = '└'
+        elif direction == 'updown': start_shape = ' '
+        else: start_shape = '├'
+
+
+        print('{0}{1}{2}{3}'.format(indent, start_shape, label, '┤'))
+
+        if type(self.left) is BinaryTree:
+            next_indent = '{0}{1}{2}'.format(indent, ' ' if 'down' in direction else '│', ' ' * len(label))
+            self.left.pretty(next_indent, direction='down')
+        else:
+            print('{0}{1}{2}└-'.format(indent, ' ' if 'down' in direction else '│', ' ' * len(label)))
+
+
+
+def decomp_to_binary_tree_b_3(decomp):
+    print("b_3")
+    print(type(decomp.first))
+    print(type(decomp.second))
+    print(decomp)
+    numblacknodes = 1
+    numwhitenodes = 0
+    numtotal = 1
+    if type(decomp.first.first) is UAtomClass:
+        left = None
+    else:
+        left = decomp.first.first
+        if left:
+            numblacknodes += left.get_attribute('numblacknodes')
+            numwhitenodes += left.get_attribute('numwhitenodes')
+            numtotal += left.get_attribute('numtotal')
+    
+    if type(decomp.second) is UAtomClass:
+        right = None
+    else:
+        right = decomp.second
+        if right:
+            numblacknodes += right.get_attribute('numblacknodes')
+            numwhitenodes += right.get_attribute('numwhitenodes')
+            numtotal += right.get_attribute('numtotal')
+
+    tree = BinaryTree(left, right, 
+        color='black', 
+        numtotal=numtotal, 
+        numblacknodes=numblacknodes, 
+        numwhitenodes=numwhitenodes
+    )
+    print(tree)
+    return tree
+
+def decomp_to_binary_tree_b_4(decomp):
+    print("b_4")
+    print(type(decomp.first))
+    print(type(decomp.second))
+    print(decomp)
+
+    numblacknodes = 1
+    numwhitenodes = 0
+    numtotal = 1
+
+    #(((X,L),U),U)
+    if type(decomp.second) is UAtomClass and type(decomp.first.second) is UAtomClass:
+        print("#(((X,L),U),U)")
+        left = decomp.first.first.first
+        if left:
+            numblacknodes += left.get_attribute('numblacknodes')
+            numwhitenodes += left.get_attribute('numwhitenodes')
+            numtotal += left.get_attribute('numtotal')
+
+        numwhitenodes += 1
+        numtotal += 1
+        right = BinaryTree(None, None, color='white')
+            
+    #(((U,U),L),X)
+    elif type(decomp.first.first) is ProdClass and type(decomp.first.first.first) is UAtomClass and type(decomp.first.first.second) is UAtomClass:
+        print("#(((U,U),L),X)")
+        numwhitenodes += 1
+        numtotal += 1
+        left = BinaryTree(None, None, color='white')
+
+        right = decomp.second
+        if right:
+            numblacknodes += right.get_attribute('numblacknodes')
+            numwhitenodes += right.get_attribute('numwhitenodes')
+            numtotal += right.get_attribute('numtotal')
+    #((X,L),X)
+    else:
+        print("#((X,L),X)")
+        print(decomp)
+        right = decomp.second
+        if right:
+            numblacknodes += right.get_attribute('numblacknodes')
+            numwhitenodes += right.get_attribute('numwhitenodes')
+            numtotal += right.get_attribute('numtotal')
+
+        left = decomp.first.first
+        if left:
+            numblacknodes += left.get_attribute('numblacknodes')
+            numwhitenodes += left.get_attribute('numwhitenodes')
+            numtotal += left.get_attribute('numtotal')
+        
+
+    tree = BinaryTree(left, right,
+        color='black', 
+        numtotal=numtotal, 
+        numblacknodes=numblacknodes, 
+        numwhitenodes=numwhitenodes
+    )
+    print(tree)
+    return tree
+    
+def decomp_to_binary_tree_w_2(decomp):
+    print("w_2")
+    print(type(decomp.first))
+    print(type(decomp.second))
+    print(decomp)
+
+    numblacknodes = 0
+    numwhitenodes = 1
+    numtotal = 1
+
+    if type(decomp.first) is UAtomClass:
+        left = None
+    else:
+        left = decomp.first
+        if left:
+            numblacknodes += left.get_attribute('numblacknodes')
+            numwhitenodes += left.get_attribute('numwhitenodes')
+            numtotal += left.get_attribute('numtotal')
+
+    if type(decomp.second) is UAtomClass:
+        right = None
+    else:
+        right = decomp.second
+        if right:
+            numblacknodes += right.get_attribute('numblacknodes')
+            numwhitenodes += right.get_attribute('numwhitenodes')
+            numtotal += right.get_attribute('numtotal')
+    tree = BinaryTree(left, right, 
+        color='white',
+        numtotal=numtotal, 
+        numblacknodes=numblacknodes, 
+        numwhitenodes=numwhitenodes
+    )
+    print(tree)
+    return tree
+
+def decomp_to_binary_tree_w_1_2(decomp):
+    """Creates either a white rooted tree with a left or a right None subtree
+    or returns a None
+    """
+    if type(decomp) is UAtomClass:
+        return None
+
+    print("w_1_2")
+    print(type(decomp.first))
+    print(type(decomp.second))
+    print(decomp)
+    numblacknodes = 1
+    numwhitenodes = 0
+    numtotal = 1
+    if type(decomp.first) is UAtomClass:
+        left = None
+    else:
+        left = decomp.first
+        if left:
+            numblacknodes += left.get_attribute('numblacknodes')
+            numwhitenodes += left.get_attribute('numwhitenodes')
+            numtotal += left.get_attribute('numtotal')
+
+    if type(decomp.second) is UAtomClass:
+        right = None
+    else:
+        right = decomp.second
+        if right:
+            numblacknodes += right.get_attribute('numblacknodes')
+            numwhitenodes += right.get_attribute('numwhitenodes')
+            numtotal += right.get_attribute('numtotal')
+
+    tree = BinaryTree(left, right, 
+        color='white',
+        numtotal=numtotal, 
+        numblacknodes=numblacknodes, 
+        numwhitenodes=numwhitenodes
+    )
+    print(tree)
+    return tree
 
 binary_tree_grammar = DecompositionGrammar()
 binary_tree_grammar.add_rules({
     'K_dy': R_b_as + R_w_as,
-    'R_b_as': (R_w * L + U) + (U + L + R_w) + (L * R_w * R_w),
-    'R_w_as': (R_b_head * U) + (U * R_b_head) + (R_b * R_b),
-    'R_b_head': (R_w_head * L * U * U) + (U * U * L * R_w_head) + (R_w_head * L * R_w_head),
-    'R_w_head': U * (R_b * U) + (U * R_b) + (R_b * R_b),
-    'R_b': Bijection((U + R_w) * L * (U + R_w), decomp_to_binary_tree_b),
-    'R_w': Bijection((U + R_b) * (U + R_b), decomp_to_binary_tree_w)
+    'R_b_as': Bijection((R_w * L * U) + (U * L * R_w) + (R_w * L * R_w), decomp_to_binary_tree_b_3),
+    'R_w_as': Bijection((R_b_head * U) + (U * R_b_head) + (R_b * R_b), decomp_to_binary_tree_w_2),
+    'R_b_head': Bijection((R_w_head * L * U * U) + (U * U * L * R_w_head) + (R_w_head * L * R_w_head), decomp_to_binary_tree_b_4),
+    'R_w_head': Bijection(U + (R_b * U) + (U * R_b) + (R_b * R_b), decomp_to_binary_tree_w_1_2),
+    'R_b': Bijection((U + R_w) * L * (U + R_w), decomp_to_binary_tree_b_3),
+    'R_w': Bijection((U + R_b) * (U + R_b), decomp_to_binary_tree_w_2)
 })
--- planarity_generation/framework_test.py
+++ planarity_generation/framework_test.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from framework.samplers.generic_samplers import *
 from framework.decomposition_grammar import *
+from framework.evaluation_oracle import Oracle
+from framework.binary_tree_decomposition import binary_tree_grammar
+from collections import deque
+
+import argparse
+
 
 # some shortcuts to make the grammar more readable
-class Oracle:
+class Oracle2(Orcale):
     # these values are for size 100
     evaluations = {
         'x0': 0.4999749994,
@@ -19,39 +25,131 @@ class Oracle:
             return 0.5
 
 
-# some shortcuts to make the grammar more readable
-Z = ZeroAtom()
-L = LAtom()
-U = UAtom()
-
-Tree = Alias('Tree')
-Bla = Alias('Bla')
-Blub = Alias('Blub')
-Blob = Alias('Blob')
-
-test_grammar = DecompositionGrammar()
-test_grammar.add_rules({
-
-    # tree is either a leaf or inner node with two children which are trees
-    'Tree': L + Tree * L * Blub,
-    'Blub': USubs(Bla, Blob),
-    'Blob': L + U + Z,
-    'Bla': LSubs(Blub, Blob),
 
 
-})
 
-# inject the oracle into the samplers
-BoltzmannSampler.oracle = Oracle()
-Alias.active_grammar = test_grammar
+def other_test():
+    # some shortcuts to make the grammar more readable
+    Z = ZeroAtom()
+    L = LAtom()
+    U = UAtom()
+    Bij = Bijection
+    Tree = Alias('Tree')
+    Bla = Alias('Bla')
+    Blub = Alias('Blub')
+    Blob = Alias('Blob')
 
-print(test_grammar.recursive_rules)
-print()
+    Tree = Alias('Tree')
 
-print(test_grammar.collect_oracle_queries('Bla', 'x', 'y'))
+    test_grammar = DecompositionGrammar()
+    test_grammar.add_rules({
 
-#for key in test_grammar.get_rules():
-    #print(test_grammar.collect_oracle_queries(key, 'x', 'y'))
+        # this is just for testing
+        # usual binary tree
+        # T = (1 + T)² * Z_L
+        # tree is either a leaf or inner node with two children which are trees
+        'Tree': L + Tree * L * Blub,
+        'Blub': USubs(Bla, Blob),
+        'Blob': L + U + Z,
+        'Bla': LSubs(Blub, Blob),
 
-# sizes = [test_grammar.sample('Tree', 'x0', 'y0').get_l_size() for _ in range(1000)]
-# sum(sizes) / len(sizes)
+    })
+
+    # inject the oracle into the samplers
+    BoltzmannSampler.oracle = Oracle2()#
+    BoltzmannSampler.active_grammar = test_grammar
+    print(test_grammar.sample('Bla', 'x0', 'y0'))
+    print(test_grammar.collect_oracle_queries('Bla', 'x', 'y'))
+
+    print(test_grammar.recursive_rules)
+
+
+    #for key in test_grammar.get_rules():
+        #print(test_grammar.collect_oracle_queries(key, 'x', 'y'))
+
+    # sizes = [test_grammar.sample('Tree', 'x0', 'y0').get_l_size() for _ in range(1000)]
+    # sum(sizes) / len(sizes)
+
+def binary_tree_test():
+
+    class BinaryTreeOracle(Oracle):
+        def __init__(self):
+            self.evaluations = {
+                'x': 0.0149875,
+                'y': 1.0,
+                'R_b_as(x,y)': 1,
+                'R_w_as(x,y)': 1,
+                'R_w(x,y)': 0.9,
+                'R_b(x,y)': 0.9,
+                'R_b_head(x,y)': 0.000001,
+                'R_w_head(x,y)': 0.9
+            }
+
+
+
+    BoltzmannSampler.oracle = BinaryTreeOracle()
+    BoltzmannSampler.active_grammar = binary_tree_grammar
+    tree = binary_tree_grammar.sample('K_dy', 'x', 'y')
+    print(tree)
+    tree.pretty()
+    print("Black Nodes: {}".format(tree.get_attribute('numblacknodes')))
+    print("White Nodes: {}".format(tree.get_attribute('numwhitenodes')))
+    print("Total Nodes: {}".format(tree.get_attribute('numtotal')))
+
+    return tree
+
+
+
+def plot_binary_tree(tree):
+    import networkx as nx
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    stack = deque([tree])
+    G=nx.empty_graph(tree._id+1)
+
+    while stack:
+        node = stack.pop()
+        label_node = node._id
+        G.nodes[label_node]['color'] = node.get_attribute('color')
+        if node.left is not None:
+            left = node.left
+            stack.append(left)
+            label_left = left._id
+            G.add_edge(label_node, label_left)
+        if node.right is not None:
+            right = node.right
+            stack.append(right)
+            label_right = right._id
+            G.add_edge(label_node, label_right)
+
+    colors = []
+    for x in nx.get_node_attributes(G, 'color').values():
+        if x is 'black':
+            colors.append('#222222')
+        else:
+            colors.append('#DDDDDD')
+
+
+    nx.draw(G, node_color=colors)
+    plt.show()
+
+def main():
+    argparser = argparse.ArgumentParser(description='Process some integers.')
+    argparser.add_argument('-b', '--binary-tree', action='store_true')
+    argparser.add_argument('--plot', action='store_true')
+    argparser.add_argument('--other', action='store_true')
+
+    args = argparser.parse_args()
+    if args.binary_tree:
+        tree = binary_tree_test()
+
+    if args.plot:
+        plot_binary_tree(tree)
+
+    if args.other:
+        other_test()
+
+
+if __name__ == '__main__':
+    main()
