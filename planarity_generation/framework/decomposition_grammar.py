6a1bf98733b74d66fab1b09ad99aa39c52054757
Tobias Winkler <tobias.winkler1@rwth-aachen.de>
2018-05-29 20:03:54 +0200

    grammars now need to init() before usage
    fixed bug in FindRecursiveRulesVisitor

--- planarity_generation/framework/decomposition_grammar.py
+++ planarity_generation/framework/decomposition_grammar.py
@@ -1,23 +1,26 @@
 from .samplers.generic_samplers import *
-
+import sys
 
 # I have decided to put the Alias sampler here because it is so closely linked to the grammar
 # An alias sampler is a sampler defined by a rule in a decomposition grammar.
 # The rule can contain the same alias sampler itself.
 # This allows to implement recursive decompositions.
 class Alias(BoltzmannSampler):
-    # attention, this is static
-    active_grammar = None
 
-    def __init__(self, alias):
+    grammar_not_initialized_error_msg = ": you have to set the grammar this Alias sampler belongs to before using it"
+
+    def __init__(self, alias, grammar = None):
         self.alias = alias
+        self.grammar = grammar
 
     def get_children(self):
-        return [self.active_grammar.get_rule(self.alias)]
+        if self.grammar is None:
+            sys.exit(self.alias + self.grammar_not_initialized_error_msg)
+        return [self.grammar.get_rule(self.alias)]
 
     # here we let the visitor decide if he wants to recurse further down into the children
     # kind of ugly but needed to avoid infinite recursion
-    # alternative: ...
+    # alternative: let the Alias sampler itself decide that
     def accept(self, visitor):
         visitor_wants_to_go_on = visitor.visit(self)
         if visitor_wants_to_go_on:
@@ -27,108 +30,134 @@ class Alias(BoltzmannSampler):
     def sampled_class(self):
         return self.alias
 
-    def oracle_query_string(self, x: str, y: str):
+    def is_recursive(self):
         # Am I a recursive?
-        if self.active_grammar.is_recursive_rule(self.alias):
+        return self.grammar.is_recursive_rule(self.alias)
+
+    def oracle_query_string(self, x: str, y: str):
+        if self.grammar is None:
+            sys.exit(self.alias + self.grammar_not_initialized_error_msg)
+        if self.is_recursive():
             return self.alias + '(' + x + ',' + y + ')'
         else:
-            return self.active_grammar.get_rule(self.alias).oracle_query_string(x, y)
+            return self.grammar.get_rule(self.alias).oracle_query_string(x, y)
 
     def get_all_oracle_queries(self, x, y):
-        if self.active_grammar.is_recursive_rule(self.alias):
+        if self.grammar is None:
+            sys.exit(self.alias + self.grammar_not_initialized_error_msg)
+        if self.grammar.is_recursive_rule(self.alias):
             return [self.oracle_query_string(x, y)]
         else:
-            return self.active_grammar.get_rule(self.alias).get_all_oracle_queries(x, y)
+            return self.grammar.get_rule(self.alias).get_all_oracle_queries(x, y)
 
     def get_eval(self, x, y):
-        if self.active_grammar.is_recursive_rule(self.alias):
+        if self.grammar is None:
+            sys.exit(self.alias + self.grammar_not_initialized_error_msg)
+        if self.grammar.is_recursive_rule(self.alias):
             return self.oracle.get(self.oracle_query_string(x, y))
         else:
-            return self.active_grammar.get_rule(self.alias).get_eval(x, y)
+            return self.grammar.get_rule(self.alias).get_eval(x, y)
 
     def sample(self, x, y):
-        return self.active_grammar.get_rule(self.alias).sample(x, y)
+        if self.grammar is None:
+            sys.exit(self.alias + self.grammar_not_initialized_error_msg)
+        return self.grammar.get_rule(self.alias).sample(x, y)
 
 
 class DecompositionGrammar:
-    def __init__(self):
-        self.rules = {}
-        self.recursive_rules = []
+    def __init__(self, rules = {}):
+        self.rules = rules
+        self.recursive_rules = None
 
-    def add_rule(self, alias, combinatorial_class):
-        self.rules[alias] = combinatorial_class
-        self.find_recursive_rules()
+    def add_rule(self, alias, sampler):
+        self.rules[alias] = sampler
 
     def add_rules(self, rules):
         # merges the dictionaries, second has higher priority
         self.rules = {**self.rules, **rules}
-        self.find_recursive_rules()
 
     def get_rules(self):
         return self.rules
 
-    def get_rule(self, key):
-        return self.rules[key]
+    def get_rule(self, alias):
+        return self.rules[alias]
 
-    def find_recursive_rules(self):
-        ## !!! ugly
-        Alias.active_grammar = self
-        ###
-        self.recursive_rules = []
-        for key in self.rules:
-            visitor = self.RecursiveRuleVisitor()
-            Alias(key).accept(visitor)
-            if key in visitor.recursive_rules:
-                self.recursive_rules.append(key)
+    def get_recursive_rules(self):
+        if self.recursive_rules is None:
+            sys.exit("You have to initialize the grammar first. Call init() after adding all your rules.")
+        return self.recursive_rules
 
     def is_recursive_rule(self, key):
+        if self.recursive_rules is None:
+            sys.exit("You have to initialize the grammar first. Call init() after adding all your rules.")
         return key in self.recursive_rules
 
-    def collect_oracle_queries(self, key, x, y):
+    def collect_oracle_queries(self, alias, x, y):
+        if alias not in self.rules:
+            sys.exit(alias + ": is not a rule in the grammar")
         visitor = self.CollectOracleQueriesVisitor(x, y)
-        Alias(key).accept(visitor)
+        self.rules[alias].accept(visitor)
         return visitor.result
 
     # samples from the rule identified by the key "alias"
     def sample(self, alias, x, y):
-        return Alias(alias).sample(x, y)
+        if alias not in self.rules:
+            sys.exit(alias + ": is not a rule in the grammar")
+        return self.rules[alias].sample(x, y)
 
-    class CollectOracleQueriesVisitor:
-        def __init__(self, x_0, y_0):
-            self.seen_alias_samplers = []
-            self.result = set([])
-            self.x = x_0
-            self.y = y_0
-            self.stack_x = []
-            self.stack_y = []
+    def init(self):
+        # initialize the alias samplers (set their referenced grammar to this grammar)
+        visitor = self.SetActiveGrammarVisitor(self)
+        for alias in self.rules:
+            #print(alias)
+            self.rules[alias].accept(visitor)
+        # find out which rules are recursive
+        self.recursive_rules = set()
+        for alias in self.rules:
+            visitor = self.FindRecursiveRulesVisitor()
+            self.rules[alias].accept(visitor)
+            self.recursive_rules |= set(visitor.get_result())
+
+
+    ### visitors ###
+
+    # sets the grammar in all Alias samplers participating in the given grammar
+    class SetActiveGrammarVisitor:
+        def __init__(self, grammar):
+            self.grammar = grammar
 
         def visit(self, sampler):
+            if isinstance(sampler, Alias):
+                sampler.grammar = self.grammar
+                visitor_wants_to_go_on = False
+                return visitor_wants_to_go_on
 
-            if self.stack_x and self.stack_x[len(self.stack_x) - 1][0] == sampler:
-                _, x = self.stack_x.pop()
-                self.x = x
+    # determines which rules are recursive
+    # todo heres a bug
+    class FindRecursiveRulesVisitor:
+        def __init__(self):
+            self.seen_alias_samplers = []
+            self.result = []
+            self.stack = []
 
-            if self.stack_y and self.stack_y[len(self.stack_y) - 1][0] == sampler:
-                _, y = self.stack_y.pop()
-                self.y = y
+        def get_result(self):
+            return self.result
 
-            if isinstance(sampler, LAtom) or isinstance(sampler, UAtom):
-                self.result.add(sampler.oracle_query_string(self.x, self.y))
-            if isinstance(sampler, LSubs):
-                self.stack_x.append((sampler.rhs, self.x))
-                self.x = sampler.rhs.oracle_query_string(self.x, self.y)
-            if isinstance(sampler, USubs):
-                self.stack_y.append((sampler.rhs, self.y))
-                self.y = sampler.rhs.oracle_query_string(self.x, self.y)
-            if isinstance(sampler, LDerFromUDer):
-                self.result.add(sampler.oracle_query_string(self.x, self.y))
-            if isinstance(sampler, UDerFromLDer):
-                self.result.add(sampler.oracle_query_string(self.x, self.y))
-            if isinstance(sampler, Transformation):
-                self.result.add(sampler.oracle_query_string(self.x, self.y))
+        def visit(self, sampler):
+            if self.stack and self.stack[-1][0] == sampler:
+                _, old_state = self.stack.pop()
+                self.seen_alias_samplers = old_state
+            # at binary samplers the path branches
+            if isinstance(sampler, BinarySampler):
+                # the visitor goes into the left child first
+                # so when we get to the right child later we can now and restore state
+                # here the list has to be copied
+                self.stack.append((sampler.rhs, list(self.seen_alias_samplers)))
             if isinstance(sampler, Alias):
                 if sampler.alias in self.seen_alias_samplers:
-                    self.result.add(sampler.oracle_query_string(self.x, self.y))
+                    # if we see it for the second time then we know it's recursive
+                    self.result.append(sampler.alias)
+                    # return False in order to not recurse down forever
                     visitor_wants_to_go_on = False
                     return visitor_wants_to_go_on
                 else:
@@ -136,17 +165,54 @@ class DecompositionGrammar:
                     visitor_wants_to_go_on = True
                     return visitor_wants_to_go_on
 
-    class RecursiveRuleVisitor:
-        def __init__(self):
+    # predicts which oracle queries will be made when sampling from the grammar
+    class CollectOracleQueriesVisitor:
+        def __init__(self, x_0, y_0):
             self.seen_alias_samplers = []
-            self.recursive_rules = []
+            self.result = set()
+            self.x = x_0
+            self.y = y_0
+            self.stack_x = []
+            self.stack_y = []
+
+        def get_result(self):
+            return self.result
 
         def visit(self, sampler):
+
+            if self.stack_x and self.stack_x[-1][0] == sampler:
+                _, x = self.stack_x.pop()
+                self.x = x
+
+            if self.stack_y and self.stack_y[-1][0] == sampler:
+                _, y = self.stack_y.pop()
+                self.y = y
+
+            if isinstance(sampler, LAtom) or isinstance(sampler, UAtom):
+                self.result.add(sampler.oracle_query_string(self.x, self.y))
+
+            if isinstance(sampler, LSubs):
+                self.stack_x.append((sampler.rhs, self.x))
+                self.x = sampler.rhs.oracle_query_string(self.x, self.y)
+
+            if isinstance(sampler, USubs):
+                self.stack_y.append((sampler.rhs, self.y))
+                self.y = sampler.rhs.oracle_query_string(self.x, self.y)
+
+            if isinstance(sampler, LDerFromUDer):
+                self.result.add(sampler.oracle_query_string(self.x, self.y))
+
+            if isinstance(sampler, UDerFromLDer):
+                self.result.add(sampler.oracle_query_string(self.x, self.y))
+
+            if isinstance(sampler, Transformation):
+                self.result.add(sampler.oracle_query_string(self.x, self.y))
+
             if isinstance(sampler, Alias):
                 if sampler.alias in self.seen_alias_samplers:
-                    # if we see it for the second time then we know it's recursive
-                    self.recursive_rules.append(sampler.alias)
-                    # return False in order to not recurse down forever
+                    if sampler.is_recursive():
+                        # i'm not very confident about this
+                        self.result.add(sampler.oracle_query_string(self.x, self.y))
                     visitor_wants_to_go_on = False
                     return visitor_wants_to_go_on
                 else:
