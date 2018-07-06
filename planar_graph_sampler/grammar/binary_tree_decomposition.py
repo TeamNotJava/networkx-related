82add26c264ba958069d90245968888595963091
Tobias Winkler <tobias.winkler1@rwth-aachen.de>
2018-07-06 19:27:49 +0200

    Moved bin tree builders to grammar file

--- planar_graph_sampler/grammar/binary_tree_decomposition.py
+++ planar_graph_sampler/grammar/binary_tree_decomposition.py
@@ -1,11 +1,83 @@
+from framework.class_builder import CombinatorialClassBuilder
 from framework.evaluation_oracle import EvaluationOracle
 from framework.decomposition_grammar import DecompositionGrammar, AliasSampler
 from framework.generic_samplers import *
-from framework.utils import bern
+from framework.utils import bern, Counter
 
-from planar_graph_sampler.bijections.binary_tree_builders import WhiteRootedBinaryTreeBuilder, \
-    BlackRootedBinaryTreeBuilder
+from planar_graph_sampler.combinatorial_classes import BinaryTree
+from planar_graph_sampler.combinatorial_classes.binary_tree import Leaf
 from planar_graph_sampler.evaluations_planar_graph import planar_graph_evals_n100
+
+# TODO Using the counter, the result sometimes is not a tree, at least when converted to a nx graph,
+# TODO the nx method 'is_tree' says so.
+counter = Counter()
+
+
+class WhiteRootedBinaryTreeBuilder(CombinatorialClassBuilder):
+    """
+    Builds white-rooted binary trees (rules 'R_w', 'R_w_head', 'R_w_as').
+    The builder is in the same file as the gramamr it belongs to because it is quite closely connected.
+    """
+
+    def __init__(self):
+        # self.node_nr = next(counter)
+        self.node_nr = 1
+
+    def u_atom(self):
+        return Leaf()
+
+    def product(self, lhs, rhs):
+        res = BinaryTree('white')
+        res.set_root_node_nr(self.node_nr)
+        # self.node_nr += next(counter)
+        self.node_nr += 2
+        res.add_left_child(lhs)
+        res.add_right_child(rhs)
+        return res
+
+
+class BlackRootedBinaryTreeBuilder(CombinatorialClassBuilder):
+    """
+    Builds black-rooted binary trees (rules 'R_b', 'R_b_head', 'R_b_as').
+    The builder is in the same file as the gramamr it belongs to because it is quite closely connected.
+    """
+
+    def __init__(self):
+        # self.node_nr = next(counter)
+        self.node_nr = 0
+
+    def l_atom(self):
+        res = BinaryTree('black')
+        res.set_root_node_nr(self.node_nr)
+        # self.node_nr += next(counter)
+        self.node_nr += 2
+        return res
+
+    def u_atom(self):
+        return Leaf()
+
+    def product(self, lhs, rhs):
+        res = None
+        if lhs.is_leaf():
+            # rhs is not a leaf.
+            assert rhs.is_black_rooted()
+            rhs.add_left_child(lhs)
+            res = rhs
+        elif rhs.is_leaf():
+            # lhs is not a leaf.
+            res = self.product(rhs, lhs).flip()
+        elif lhs.is_white_rooted() and rhs.is_black_rooted():
+            # Both are not leaves.
+            rhs.add_left_child(lhs)
+            res = rhs
+        elif rhs.is_white_rooted() and lhs.is_black_rooted():
+            res = self.product(rhs, lhs).flip()
+
+        if res is None:
+            print()
+        assert res is not None
+        assert res.get_root_color() is 'black'
+        return res
 
 
 def binary_tree_grammar():
