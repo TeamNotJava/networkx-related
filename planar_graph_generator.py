b7a88baa6ff6a890a806d427edb5e7285af9a0e5
Petrov <petrov.pepi@yahoo.com>
2018-08-15 14:39:45 +0200

    Time/number of tries statistics script.

--- planar_graph_generator.py
+++ planar_graph_generator.py
@@ -11,6 +11,7 @@
 #           Petre Petrov <petrepp4@gmail.com>
 #           Rudi Floren <rudi.floren@gmail.com>
 #           Tobias Winkler <tobias.winkler1@rwth-aachen.de>
+from numpy import diff
 
 from planar_graph_sampler.grammar.planar_graph_decomposition import  planar_graph_grammar, bij_connected_comps
 from framework.evaluation_oracle import EvaluationOracle
@@ -18,6 +19,7 @@ from framework.generic_samplers import BoltzmannSamplerBase
 from framework.generic_classes import SetClass
 from planar_graph_sampler.evaluations_planar_graph import planar_graph_evals
 import networkx as nx
+import datetime
 
 class PlanarGraphGenerator:
 
@@ -60,7 +62,7 @@ class PlanarGraphGenerator:
             lower_bound = node_number - (node_number * variance/100)
             upper_bound = node_number + (node_number * variance/100)
 
-        BoltzmannSamplerBase.oracle = EvaluationOracle.get_best_oracle_for_size(node_number, planar_graph_evals)     
+        BoltzmannSamplerBase.oracle = EvaluationOracle.get_best_oracle_for_size(node_number, planar_graph_evals)
 
 
         grammar = planar_graph_grammar()
@@ -69,7 +71,7 @@ class PlanarGraphGenerator:
         curr_node_number = 0
         
         planar_graph = grammar.sample_iterative('G_dx_dx', 'x', 'y')
-        
+
         if variance != 100:
             curr_node_number = planar_graph.l_size
             while(curr_node_number < lower_bound or curr_node_number > upper_bound):
@@ -79,6 +81,78 @@ class PlanarGraphGenerator:
         # Transform to networkx graph
         gnx = bij_connected_comps(planar_graph)
         return gnx
+
+
+    def generate_planar_graph_for_profiling(self, node_number, variance):
+        """This function generates a random planar graph using Boltzmann
+        samplers with fixed number of nodes with the possibility to
+        define a margin in which the number of nodes of the sampled
+        planar graph can vary.
+
+        Parameters
+        ----------
+        node_number: int
+            the desired number of nodes the graph should have
+        variance: float
+            the variance for the number of nodes in percent
+
+        Returns
+        -------
+        graph: NetworkX
+            a random networkx planar graph
+
+        Notes
+        -----
+        This algorithm makes use of the fact that every planar graph can
+        be decomposed into a set of connected planar graphs which in
+        turn can be obtained by using several bijection and rejection
+        steps starting with bicolored binary trees. For every step one
+        creates a Boltzmann sampler that produces a certain random
+        object with uniform probability. Combining all the samplers,
+        one obtain a Boltzmann sampler for planar graphs [1].
+
+        References
+        ----------
+        .. [1] Eric Fusy:
+            Uniform random sampling of planar graphs in linear time
+        """
+        # If the user specifies a variance of 100, then there are no
+        #  lower/upper bounds
+        if variance != 100:
+            lower_bound = node_number - (node_number * variance / 100)
+            upper_bound = node_number + (node_number * variance / 100)
+
+        BoltzmannSamplerBase.oracle = EvaluationOracle.get_best_oracle_for_size(node_number, planar_graph_evals)
+
+        grammar = planar_graph_grammar()
+        grammar.init()
+        grammar.precompute_evals('G_dx_dx', 'x', 'y')
+        curr_node_number = 0
+
+        planar_graph = grammar.sample_iterative('G_dx_dx', 'x', 'y')
+
+        lower_bound_restriction_error_count = 0
+        upper_bound_restricctio_error_count = 0
+        if variance != 100:
+            curr_node_number = planar_graph.l_size
+            while (curr_node_number < lower_bound or curr_node_number > upper_bound):
+                if curr_node_number < lower_bound:
+                    lower_bound_restriction_error_count += 1
+                else:
+                    upper_bound_restricctio_error_count += 1
+
+                print(lower_bound_restriction_error_count)
+
+                planar_graph = grammar.sample_iterative('G_dx_dx', 'x', 'y')
+                curr_node_number = planar_graph.l_size
+
+
+        print("Lower bound restruction errors: %s " % lower_bound_restriction_error_count)
+        print("Upper bound restruction errors: %s " % upper_bound_restricctio_error_count)
+
+        # Transform to networkx graph
+        gnx = bij_connected_comps(planar_graph)
+        return gnx, lower_bound_restriction_error_count, upper_bound_restricctio_error_count
 
     def generate_planar_graph_fixed_nodes_edges(self, node_num, edge_num, node_var, edge_var):
         """This function samples random planar graphs using Boltzmann
--- profiling.py
+++ profiling.py
@@ -1,7 +1,32 @@
 from planar_graph_generator import PlanarGraphGenerator
 import networkx as nx
+import cProfile
+import time
+
+
+def __generate_planar_graph():
+    graphs_sizes = [100, 500, 1000, 1500, 2000, 3000, 4000, 5000, 7500, 1000]
+    variances = [50]
+    how_many_graphs_per_size = 10
+
+    with open("time_statistics.txt", "a") as myfile:
+        for N in graphs_sizes:
+            for variance in variances:
+                for i in range(how_many_graphs_per_size):
+                    start = time.time()
+
+                    generator = PlanarGraphGenerator()
+                    gnx, lower_bound_errors, upper_bound_errors = generator.generate_planar_graph_for_profiling(N, variance)
+
+                    end = time.time()
+                    time_in_seconds = end - start
+                    print(nx.info(gnx))
+                    myfile.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % (N, variances,
+                        gnx.number_of_nodes(), gnx.number_of_edges(), time_in_seconds, lower_bound_errors, upper_bound_errors ))
+                    myfile.flush()
+
+
 
 if __name__ == '__main__':
-    generator = PlanarGraphGenerator()
-    gnx = generator.generate_planar_graph(1000, 50)
-    print(nx.info(gnx))
+    #cProfile.run('__generate_planar_graph()')
+    __generate_planar_graph()
