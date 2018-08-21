dc1d73fe5ea0ac109223a9ad0341c27e714c2bc7
Valkum <rudi.floren@gmail.com>
2018-08-21 20:46:41 +0200

    Add plotting to random generatod planar graph demo

--- demo_planar_generation.py
+++ demo_planar_generation.py
@@ -1,9 +1,15 @@
 from planar_graph_generator import PlanarGraphGenerator
 import networkx as nx
+import matplotlib.pyplot as plt
 
 
 
 if __name__ == '__main__':
     sampler = PlanarGraphGenerator()
-    graph = sampler.generate_planar_graph(1000, 50)
+    graph = sampler.generate_planar_graph(100, 50)
     print(nx.info(graph))
+
+    pos = nx.combinatorial_embedding_to_pos(graph)
+    nx.draw(graph, pos, width=2, node_size=30)
+    plt.savefig("planar_graph.png", format="PNG")
+    plt.show()
